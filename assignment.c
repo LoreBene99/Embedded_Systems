82d3bea74688a148bee4fe3ad0192aab8f8a9164
Marco <marcomacchia99@gmail.com>
2022-11-16 15:57:38 +0100

    aggiornato file assignment.c

--- assignment.c
+++ assignment.c
@@ -1,6 +1,6 @@
 /*
  * File:   Assignment.c
- * Author: Marco
+ * Author: Marco Macchia, Lorenzo Benedetti, Samuele Pedrazzi, Matteo Aicardi, Davide Bruzzo
  *
  * Created on 6 novembre 2022, 15.59
  */
@@ -42,7 +42,17 @@
 
 #define TIMER1 1
 #define TIMER2 2
+#define TIMER3 3
 #define SIZE 16
+
+
+//struct for circular buffer
+
+typedef struct {
+    char data[SIZE];
+    int read_idx;
+    int write_idx;
+} circularBuffer;
 
 //setup the period of a timer
 void tmr_setup_period(int timer, int ms);
@@ -60,17 +70,15 @@ void send_lcd(char character);
 void send_uart(int counter);
 //setup board and simulation
 void setup_board(int counter);
-//UART2 receiver interrupt routine
-void __attribute__((__interrupt__, __auto_psv__)) _U2RXInterrupt();
+//convert int to string
+void my_itoa(int num, char buffer[]);
 
-//9600 baud significa invio di 9600 simboli (bit) al secondo. Un char in UART
-//sono 10 bit, 8 di char + start e stop. Quindi 960 caratteri al secondo, 
-//circa 1 ogni 1ms. Durante l'algoritmo possiamo ricevere quindi al massimo 7 char
-//che dovranno essere scritti.
-char circular_buffer[SIZE];
-int read_idx = 0;
-int write_idx = 0;
+//global circular buffer
+volatile circularBuffer cb;
+//counter that counts how many char the main loop should read from che circular buffer
 int to_be_read_counter = 0;
+//flag raised when s5 is pressed
+int s5_pressed = 0;
 
 int main(void) {
 
@@ -84,7 +92,6 @@ int main(void) {
 
     //main loop
     while (1) {
-
         //wait main period
         tmr_wait_period(TIMER2);
 
@@ -92,9 +99,10 @@ int main(void) {
         tmr_wait_ms(TIMER1, 7);
 
         //if button S5 has been pressed
-        if (IFS0bits.INT0IF) {
-            //reset INT0 flag
-            IFS0bits.INT0IF = 0;
+        if (s5_pressed) {
+
+            //reset flag
+            s5_pressed = 0;
 
             //send number of char recv via uart
             send_uart(counter);
@@ -118,15 +126,20 @@ int main(void) {
         }
 
         //if there is a character that has to be read from the buffer
-        if (to_be_read_counter) {
+        while (to_be_read_counter > 0) {
 
             //read incoming character
-            char received = circular_buffer[read_idx++];
+            char received = cb.data[cb.read_idx++];
             //when end is reached start again from the beggining of the array
-            read_idx = read_idx % SIZE;
+            cb.read_idx = cb.read_idx % SIZE;
+
+            //disable UART2 Interrupt
+            IEC1bits.U2RXIE = 0;
             //increment counter for char that has to be read
             to_be_read_counter--;
 
+            //re-enable UART2 interrupt
+            IEC1bits.U2RXIE = 1;
 
             //if CR or LF is received clear first row
             if (received == '\r' || received == '\n') {
@@ -149,7 +162,6 @@ int main(void) {
 
                 //if an overflow is occurred
                 if (U2STAbits.OERR == 1) {
-                    //[...]
                     //reset overflow flag and discard all char
                     U2STAbits.OERR = 0;
                 }
@@ -161,64 +173,96 @@ int main(void) {
                 counter++;
                 counter_first_row++;
 
-                //print the second row of lcd
-                print_second_row_lcd(counter);
+                //max value of counter is 999. If exceeded reset
+                counter = counter % 1000;
 
-                //move cursor to the correct position in the first row
-                send_lcd(0x80 + counter_first_row);
             }
         }
+        //print the second row of lcd
+        print_second_row_lcd(counter);
+
+        //move cursor to the correct position in the first row
+        send_lcd(0x80 + counter_first_row);
     }
     return 0;
 }
 
 void setup_board(int counter) {
+
     //time to initialize lcd
     tmr_wait_ms(TIMER1, 1000);
 
-    //setup timer to synchronize main to 100hz
-    tmr_setup_period(TIMER2, 10);
-
     // set UART2
-    U2BRG = 11; // (7372800 / 4) / (16 * 9600) ? 1
-    U2MODEbits.UARTEN = 1; // enable UART
-    U2STAbits.UTXEN = 1; // enable U2TX (must be after UARTEN)
+    // (7372800 / 4) / (16 * 9600) - 1
+    U2BRG = 11;
+    // enable UART
+    U2MODEbits.UARTEN = 1;
+    // enable U2TX
+    U2STAbits.UTXEN = 1;
     U2STAbits.URXISEL = 1;
 
     //enable UART receiver interrupt
     IEC1bits.U2RXIE = 1;
 
     //set SPI1
-    SPI1CONbits.MSTEN = 1; // master mode
-    SPI1CONbits.MODE16 = 0; // 8?bit mode
-    SPI1CONbits.PPRE = 3; // 1:1 primary prescaler
-    SPI1CONbits.SPRE = 6; // 1:2 secondary prescaler
-    SPI1STATbits.SPIEN = 1; // enable SPI
+    // master mode
+    SPI1CONbits.MSTEN = 1;
+    // 8 bit mode
+    SPI1CONbits.MODE16 = 0;
+    // 1:1 primary prescaler
+    SPI1CONbits.PPRE = 1;
+    // 1:2 secondary prescaler 
+    SPI1CONbits.SPRE = 6;
+    SPI1STATbits.SPIEN = 1;
+    // enable SPI
+
+    //enable INT0 interrupt (button S5)
+    IEC0bits.INT0IE = 1;
+
+    //set S5 button (E8 pin) as input
+    TRISEbits.TRISE8 = 1;
 
     //print the second row of lcd
     print_second_row_lcd(counter);
     //move cursor to first position of first row
     send_lcd(0x80);
+
+    //setup timer to synchronize main to 100hz
+    tmr_setup_period(TIMER2, 10);
 }
 
 void tmr_setup_period(int timer, int ms) {
 
-    int pr = (((7372800 / 4) / 64) / (1000.0 / ms)); // (7.3728 MHz / 4) / 64 / (1000/ms)
+    // (7.3728 MHz / 4) / 64 / (1000/ms)
+    int pr = (((7372800 / 4) / 64) / (1000.0 / ms));
 
     switch (timer) {
         case 1:
             TMR1 = 0;
             PR1 = pr;
             T1CONbits.TCKPS = 2;
-            IFS0bits.T1IF = 0; // set interrupt to zero
-            T1CONbits.TON = 1; // starts the timer!
+            // set interrupt flag to zero
+            IFS0bits.T1IF = 0;
+            // starts the timer
+            T1CONbits.TON = 1;
             break;
         case 2:
             TMR2 = 0;
             PR2 = pr;
-            T1CONbits.TCKPS = 2;
-            IFS0bits.T2IF = 0; //set interrupt to zero
-            T2CONbits.TON = 1; // starts the timer!
+            T2CONbits.TCKPS = 2;
+            // set interrupt flag to zero
+            IFS0bits.T2IF = 0;
+            // starts the timer
+            T2CONbits.TON = 1;
+            break;
+        case 3:
+            TMR3 = 0;
+            PR3 = pr;
+            T3CONbits.TCKPS = 2;
+            // set interrupt flag to zero
+            IFS0bits.T3IF = 0;
+            // starts the timer
+            T3CONbits.TON = 1;
             break;
         default:
             break;
@@ -230,13 +274,15 @@ void tmr_wait_period(int timer) {
         case 1:
             while (IFS0bits.T1IF == 0) {
             }
-            IFS0bits.T1IF = 0; //reset flag
+            //reset flag
+            IFS0bits.T1IF = 0;
 
             break;
         case 2:
             while (IFS0bits.T2IF == 0) {
             }
-            IFS0bits.T2IF = 0; //reset flag
+            //reset flag
+            IFS0bits.T2IF = 0;
             break;
         default:
             break;
@@ -249,11 +295,13 @@ void tmr_wait_ms(int timer, int ms) {
     tmr_wait_period(timer);
     switch (timer) {
         case 1:
-            T1CONbits.TON = 0; // stops the timer!
+            // stops the timer
+            T1CONbits.TON = 0;
             TMR1 = 0;
             break;
         case 2:
-            T2CONbits.TON = 0; // stops the timer!
+            // stops the timer
+            T2CONbits.TON = 0;
             TMR2 = 0;
             break;
         default:
@@ -268,6 +316,7 @@ void clear_first_row_lcd() {
     send_lcd(0x80);
 
     for (int i = 0; i < 16; i++) {
+        //write empty char on the LCD
         send_lcd(' ');
     }
 
@@ -277,11 +326,7 @@ void clear_first_row_lcd() {
 
 void print_second_row_lcd(int counter) {
     //buffer for printing on LCD
-    char buffer[16];
-
-    //create string
-    sprintf(buffer, "Char Recv: %d%d%d", counter / 100, (counter / 10) % 10, counter % 10);
-
+    char buffer[16] = "Char Recv: ";
 
     //move cursor second row
     send_lcd(0xC0);
@@ -290,6 +335,19 @@ void print_second_row_lcd(int counter) {
     for (int i = 0; buffer[i] != '\0'; i++) {
         send_lcd(buffer[i]);
     }
+
+    //convert counter in char
+    my_itoa(counter, buffer);
+
+    //write on lcd
+    for (int i = 0; buffer[i] != '\0'; i++) {
+        send_lcd(buffer[i]);
+    }
+
+    //wipe remaining char in second row
+    for (int i = 0; i < 2 && counter < 100; i++)
+        send_lcd(' ');
+
 }
 
 void send_lcd(char character) {
@@ -304,10 +362,11 @@ void send_lcd(char character) {
 void send_uart(int counter) {
 
     //buffer for printing on LCD
-    char buffer[3];
+    char buffer[4];
+    my_itoa(counter, buffer);
 
     //create string
-    sprintf(buffer, "%d", counter);
+    //sprintf(buffer, "%d", counter);
 
     for (int i = 0; buffer[i] != '\0'; i++)
         //send via uart the number of characters received
@@ -316,15 +375,78 @@ void send_uart(int counter) {
 
 void __attribute__((__interrupt__, __auto_psv__)) _U2RXInterrupt
 () {
+    // reset interrupt flag
+    IFS1bits.U2RXIF = 0;
+
+    //read incoming char
+    char received = U2RXREG;
+
+    //if there are empty spaces on the circular buffer
     if (to_be_read_counter < SIZE) {
-        // reset interrupt flag
-        IFS1bits.U2RXIF = 0;
-        //read incoming char
-        circular_buffer[write_idx++] = U2RXREG;
+        //store incoming char on circular buffer
+        cb.data[cb.write_idx++] = received;
         //when end is reached start again from the beggining of the array
-        write_idx = write_idx % SIZE;
+        cb.write_idx = cb.write_idx % SIZE;
         //increment counter for char that has to be read
         to_be_read_counter++;
     }
 
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _INT0Interrupt
+() {
+    // reset interrupt flag
+    IFS0bits.INT0IF = 0;
+
+    //disable INT0 interrupt
+    IEC0bits.INT0IE = 0;
+
+    //start timer 3
+    tmr_setup_period(TIMER3, 30);
+
+    //enable timer 3 interrupt
+    IEC0bits.T3IE = 1;
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _T3Interrupt
+() {
+    // reset interrupt T3 flag
+    IFS0bits.T3IF = 0;
+
+    // reset interrupt INT0 flag
+    IFS0bits.INT0IF = 0;
+
+    //stop T3 timer
+    T3CONbits.TON = 0;
+    TMR3 = 0;
+
+    //disable T3 interrupt
+    IEC0bits.T3IE = 0;
+
+    //enable INT0 interrupt
+    IEC0bits.INT0IE = 1;
+
+    if (PORTEbits.RE8) {
+        //raise button pressed flag
+        s5_pressed = 1;
+    }
+}
+
+void my_itoa(int num, char buffer[]) {
+
+    //convert number into a string using ascii char codes
+    
+    if (num < 10) {
+        buffer[0] = 48 + num % 10;
+        buffer[1] = '\0';
+    } else if (num < 100) {
+        buffer[0] = 48 + (num / 10) % 10;
+        buffer[1] = 48 + num % 10;
+        buffer[2] = '\0';
+    } else {
+        buffer[0] = 48 + num / 100;
+        buffer[1] = 48 + (num / 10) % 10;
+        buffer[2] = 48 + num % 10;
+        buffer[3] = '\0';
+    }
 }
